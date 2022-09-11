e3daf74060babb5a7a7baa1275f18f60964a8e48
AUBREY WIDJAYA <aubreywidjaya2000@gmail.com>
2022-09-11 16:41:03 +0100

    web app progress

--- src/webapp/pages/3_❔_about.py
+++ src/webapp/pages/3_❔_about.py
@@ -1,7 +1,9 @@
 import streamlit as st
-st.title("About Page")
+st.set_page_config(layout="wide", page_title="Project CAV²R", page_icon="🕵️‍♀️") 
+st.title("Project CAV²R⛏️")
 
 st.header("Want to know more?")
-st.subheader("This web page explains the purpose of the website and what all of the sections in the navigation are for")
-st.subheader("In the main page, users can select one YouTube channel in the drop down menu then navigate to recommender page for recommendations of videos starting from the video with the most positive sentiment or navigate to analyzer page for sentiment analyses and visualizations of your chosen channel.")
+st.subheader("This web page explains the purpose and functions of the website and what all of the sections in the navigation are for")
+st.text("The name of this web app is an acronym which stands for Comment Analyzer, Visualizer & Video Recommender. Analyzer and Recommender are this app's main features")
+st.write("In the main page, users can select one YouTube channel in the drop down menu then navigate to recommender page for recommendations of videos starting from the video with the most positive sentiment or navigate to analyzer page for sentiment analyses and visualizations of your chosen channel.")
 st.caption("Aubrey Widjaya's Dissertation")
