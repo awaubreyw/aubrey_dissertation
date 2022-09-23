d6bf75eee3a8a676f8849841ae3b67396a94aaf6
awaubreyw <aubreywidjaya2000@gmail.com>
2022-09-23 13:19:22 +0100

    more citations

--- src/webapp/pages/3_❔_about.py
+++ src/webapp/pages/3_❔_about.py
@@ -45,6 +45,8 @@ with st.expander("More"):
 
     McKinney, W. (2010, June). Data structures for statistical computing in python. In Proceedings of the 9th Python in Science Conference (Vol. 445, No. 1, pp. 51-56).
 
+    Satyanarayan, A., Moritz, D., Wongsuphasawat, K., & Heer, J. (2016). Vega-lite: A grammar of interactive graphics. IEEE transactions on visualization and computer graphics, 23(1), 341-350.
+
     The pandas development team. pandas-dev/pandas: Pandas [Computer software]. https://github.com/pandas-dev/pandas
 
     VanderPlas, J., Granger, B., Heer, J., Moritz, D., Wongsuphasawat, K., Satyanarayan, A., Lees, E., Timofeev, I., Welsh, B. & Sievert, S. (2018). Altair: interactive statistical visualizations for Python. Journal of open source software, 3(32), 1057.
