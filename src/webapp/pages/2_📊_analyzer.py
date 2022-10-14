d60cfb1e6644935e9f74aea5b38a9e13498fd70e
awaubreyw <aubreywidjaya2000@gmail.com>
2022-10-14 13:15:13 +0100

    cache

--- src/webapp/pages/2_📊_analyzer.py
+++ src/webapp/pages/2_📊_analyzer.py
@@ -37,7 +37,7 @@ video_stats = stats['video_data']
 
 
 #function for first set of cola and colb
-@st.cache(suppress_st_warning=True, allow_output_mutation=True)
+@st.cache(allow_output_mutation=True)
 def visualize_before_sentiment(order: str, col:str):
     st.subheader(f"""{choice} Top 10 Videos\n\nordered by {order}""")
     
